aac328df7a096a32de3e22e1ae0a863f9d67d0fe
ganeshsamarth <47754795+ganeshsamarth@users.noreply.github.com>
2019-08-17 10:49:16 +0530

    Create data_manip.py

---
+++ data_manip.py
@@ -0,0 +1,39 @@
+import os
+from PIL import Image
+import random
+import numpy as np
+import h5py
+input_dir='C:\\Users\\ganeshsamarth\\Desktop\\facebank2'
+
+dataset=list()
+
+for i,actors in enumerate(os.listdir(input_dir)):
+    items_list=list()
+    for images in os.listdir(input_dir+'\\'+actors):
+        items_list.append(input_dir+'\\'+actors+'\\'+images)
+        items_list.append(i)
+        print(i)
+        dataset.append(items_list)
+
+print(len(dataset))
+random.shuffle(dataset)
+
+X=[]
+Y=[]
+count=0
+for line in dataset:
+    path=line[0]
+    label=line[1]
+    img=Image.open(path)
+    np_image=np.array(img)
+    X.append(np_image)
+    Y.append(label)
+    count+=1
+    print(count)
+
+
+
+h5f = h5py.File(input_dir+'\\'+'training_data.h5', 'w')
+h5f.create_dataset("X", data=X)
+h5f.create_dataset("Y", data=Y)
+h5f.close()
